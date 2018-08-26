fe6ce74e63341c92c098d6f5dfb1ecdcb652e583
Alex Kott <tetlokax@gmail.com>
2018-08-26 15:46:35 +0530

    fetch content instead of injecting it

--- src/render.js
+++ src/render.js
@@ -1,11 +1,17 @@
+if (! window._babelPolyfill) {
+  require('@babel/polyfill');
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import transformToRoutes from './transformToRoutes';
-
 import SissiPage from './SissiPage';
 
-export default (EntryComponent, content) => {
+export default async (EntryComponent) => {
+  const response = await fetch('/sissi/__content__');
+  const content = await response.json();
+
   ReactDOM.render(
     <SissiPage routes={transformToRoutes(content)}>
       <EntryComponent />
